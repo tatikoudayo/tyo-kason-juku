0078c6f46b6a1bb90515c50d486e12f841dec98c
tatikoudayo <tachibana.koki.ti0@is.naist.jp>
2021-09-17 05:00:09 +0900

    ユーザの作成

--- wp-content/plugins/tyo-kason-juku-customize/class-tyo-kason-juku-customize.php
+++ wp-content/plugins/tyo-kason-juku-customize/class-tyo-kason-juku-customize.php
@@ -1,4 +1,9 @@
 <?php
 
 class Tyo_Kason_Juku_Customize {
+
+	public function __construct() {
+		require_once TYO_KASON_JUKU_CUSTOMIZE_PATH . '/inclueds/system/create_user.php';
+		new Tyo_Kason_Juku_Create_User();
+	}
 }
--- wp-content/plugins/tyo-kason-juku-customize/tyo-kason-juku-customize.php
+++ wp-content/plugins/tyo-kason-juku-customize/tyo-kason-juku-customize.php
@@ -31,7 +31,7 @@ function tyo_kason_juku_customize_init() {
 	require_once TYO_KASON_JUKU_CUSTOMIZE_PATH . '/class-tyo-kason-juku-customize.php';
 	require_once TYO_KASON_JUKU_CUSTOMIZE_PATH . '/inclueds/define.php';
 
-	// new Naist_Yamato_Customize();
+	new Tyo_Kason_Juku_Customize();
 }
 
 add_action( 'plugins_loaded', 'tyo_kason_juku_customize_init', 10 );
